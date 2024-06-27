54310964b74163cfd5393d36880ec2b50cf2fd0e
andrea-lk <andrealk@stanford.edu>
2024-06-27 16:58:34 -0400

    Update and rename create a GUI to GUI.py

---
+++ GUI.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+import subprocess
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 800
+
+
+def main():
+    # create the window
+    window = tk.Tk()
+    geometry = f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}"
+    window.geometry(geometry)
+    window.title("Drone GPS Coordinates")
+
+    # create the input boxes and buttons
+    entries = create_input_boxes(window)
+    make_buttons(window, entries)
+
+    window.mainloop()
+
+
+def create_input_boxes(window):
+    labels_texts = ["Enter Turntable Latitude: ", "Enter Turntable Longitude: ", "Enter Drone Latitude: ", "Enter Drone Longitude: "]
+    entries = []
+
+    for i, label_text in enumerate(labels_texts):
+        label = tk.Label(window, text=label_text, font=("Comic Sans", 20))
+        label.grid(row=i, column=0, padx=10, pady=10, sticky='E')
+
+        entry = tk.Entry(window, font=("Comic Sans", 20))
+        entry.grid(row=i, column=1, padx=10, pady=10)
+        entries.append(entry)
+
+    return entries
+
+
+def make_buttons(window, entries):
+    submit_button = tk.Button(window, text="Submit", command=lambda: submit(entries))
+    submit_button.grid(row=len(entries), column=0, padx=10, pady=10, sticky='E')
+
+    clear_button = tk.Button(window, text="Clear", command=lambda: clear(entries))
+    clear_button.grid(row=len(entries), column=1, padx=10, pady=10, sticky='W')
+
+
+def submit(entries):
+    turntable_lat = entries[0].get()
+    turntable_long = entries[1].get()
+    drone_lat = entries[2].get()
+    drone_long = entries[3].get()
+
+    print(f"Turntable GPS coordinates:\nLatitude = {turntable_lat},\nLongitude = {turntable_long}")
+    print(f"Drone GPS coordinates:\nLatitude = {drone_lat},\nLongitude = {drone_long}")
+
+    with open("coordinates.txt", "w") as file:
+        file.write(f"{turntable_lat},{turntable_long},{drone_lat},{drone_long}")
+
+    # Run calculations.py
+    subprocess.run(["python", "calculations.py"])
+
+    # Run turntable.py
+    subprocess.run(["python", "turn_table.py"])
+
+def clear(entries):
+    for entry in entries:
+        entry.delete(0, tk.END)
+
+if __name__ == "__main__":
+    main()
